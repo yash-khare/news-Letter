1cab07b6952eb8affa03e498399cb34e340dbf9b
yashzkhare <88205384+yashzkhare@users.noreply.github.com>
2021-10-04 19:02:20 +0530

    end

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.post("/failure",function(req,res){
 })
 
 
-app.listen(process.env.Port || 3000,function(){
+app.listen(process.env.PORT || 3000,function(){
   console.log("server is up");
 });
 
